cc3b4401bc7fef5ecd06c0f40b0edf01de0a12f2
Eric Arbour <arboureric@gmail.com>
2018-03-03 16:51:40 -0500

    up through webpack server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import _ from 'lodash';
+import './style.less';
+import Icon from './react.png';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  var btn = document.createElement('button');
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
